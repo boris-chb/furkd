d5e679c478a4da42c79da8e49edf96baa736b1f9
boris-chb <47457848+boris-chb@users.noreply.github.com>
2024-05-31 15:08:31 +0100

    temporary fix

--- rabotaem.js
+++ rabotaem.js
@@ -1755,7 +1755,7 @@ let action_ = {
 
 let questionnaire_ = {
   setAnswers(answers) {
-    // BUG TEMPORARY FIX labellingGraph.ph
+    // BUG TEMPORARY FIX labellingGraph.oh
     if (!dom_.questionnaire) throw new Error('[i] Questionnaire Not Rendered');
 
     // questionnaire answering logic
@@ -1764,8 +1764,8 @@ let questionnaire_ = {
     });
 
     if (
-      !dom_.questionnaire.labellingGraph.ph ||
-      dom_.questionnaire.labellingGraph.ph.size === 0
+      !dom_.questionnaire.labellingGraph.oh ||
+      dom_.questionnaire.labellingGraph.oh.size === 0
     ) {
       throw new Error(
         'Questions not Answered!',
@@ -1774,7 +1774,7 @@ let questionnaire_ = {
     }
 
     console.log('💾 Saving questionnaire. Answers:');
-    return dom_.questionnaire.labellingGraph.ph;
+    return dom_.questionnaire.labellingGraph.oh;
   },
   generateAnswers(policyId = '3039', veGroup = store_.selectedVEGroup) {
     const answers = {};
